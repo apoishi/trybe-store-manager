719bb498da3124ffa0367e64a8e74f164d68bf7a
Ariane Padilha <ariane.padilha@outlook.com.br>
2022-10-15 12:23:55 -0300

    🎉  Initial commit

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ const app = express();
 app.get('/', (_request, response) => {
   response.send();
 });
-
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 
