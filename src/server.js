b4e2ec0220b018aeb3dd67c0129b9b61156d30f9
Ariane Padilha <ariane.padilha@outlook.com.br>
2022-10-17 13:52:30 -0300

    🔥 Removed code from server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const connection = require('./models/db/connection');
+// const connection = require('./models/db/connection');
 require('dotenv').config();
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
@@ -8,8 +8,8 @@ app.listen(process.env.PORT, async () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
 
   // O código abaixo é para testarmos a comunicação com o MySQL
-  const [result] = await connection.execute('SELECT 1');
-  if (result) {
-    console.log('MySQL connection OK');
-  }
+  // const [result] = await connection.execute('SELECT 1');
+  // if (result) {
+  //   console.log('MySQL connection OK');
+  // }
 });
