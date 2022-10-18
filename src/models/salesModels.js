c37ba4a4165a461e9d96179a7cba55a4f80907bd
Ariane Padilha <ariane.padilha@outlook.com.br>
2022-10-18 13:37:11 -0300

    ✨ Req 08 - 09 - models, tests and mocks for sales

---
+++ src/models/salesModels.js
@@ -0,0 +1,31 @@
+const camelize = require('camelize');
+const connection = require('./db/connection');
+
+const findAll = async () => {
+  const [result] = await connection.execute(
+    `SELECT sp.sale_id, s.date, sp.product_id, sp.quantity
+     FROM StoreManager.sales_products AS sp
+     INNER JOIN StoreManager.sales AS s
+     ON s.id = sp.sale_id
+     ORDER BY sp.sale_id, sp.product_id;`,
+     );
+  return camelize(result);
+};
+
+const findById = async (saleId) => {
+  const [result] = await connection.execute(
+    `SELECT s.date, sp.product_id, sp.quantity
+     FROM StoreManager.sales_products AS sp
+     INNER JOIN StoreManager.sales AS s
+     ON s.id = sp.sale_id
+     WHERE sp.sale_id = ?
+     ORDER BY sp.sale_id, sp.product_id;`,
+     [saleId],
+  );
+  return camelize(result);
+};
+  
+module.exports = {
+  findAll,
+  findById,
+};
